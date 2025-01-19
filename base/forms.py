b479aab01de286b31bf4e8b74e92439153cfc4ad
Fcontice <fcontice@uncc.edu>
2025-01-19 18:26:51 -0500

    timestamp 2:06:22

---
+++ base/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from base.models import Room, Topic, Message
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = [ 'participants']
--- studyBuddy/urls.py
+++ studyBuddy/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
+
 from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home, name="home"),
-    path('room/<str:pk>', views.room, name="room"),
-
+    path('', include('base.urls')),
+    
 ]
+
